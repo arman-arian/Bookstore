d66ad7a8457f012bd09a53a69d328fb68c5da951
arman-arian <arman.hassanpoor2000@gmail.com>
2022-07-27 06:23:09 +0430

    update domain model

--- src/main/java/com/tosan/bookstore/models/Book.java
+++ src/main/java/com/tosan/bookstore/models/Book.java
@@ -1,33 +1,45 @@
 package com.tosan.bookstore.models;
 
-import java.time.LocalDate;
+import javax.persistence.*;
+import java.time.*;
 
+@Entity
+@Table(name = "Books")
 public class Book {
+    @Id
+    @GeneratedValue
     private long id;
 
+    @Column(length=200, nullable = false, unique = true)
     private String title;
 
+    @Column(length=400, nullable = false)
     private String author;
 
+    @Column(length=20)
+    private String isbn;
+
+    private long price;
+
+    @Column(length=200)
     private String pubName;
 
     private LocalDate pubDate;
 
     private int pubYear;
 
-    private String isbn;
-
-    private long price;
-
+    @Column(length=800)
     private String description;
 
     private boolean isDeleted;
 
-    private LocalDate createdOn;
+    private LocalDateTime createdOn;
 
+    @Column(length=40)
     private String createdBy;
 
-    private LocalDate modifiedOn;
+    private LocalDateTime modifiedOn;
 
+    @Column(length=40)
     private String modifiedBy;
 }
--- src/main/java/com/tosan/bookstore/models/User.java
+++ src/main/java/com/tosan/bookstore/models/User.java
@@ -1,15 +1,32 @@
 package com.tosan.bookstore.models;
 
+import javax.persistence.*;
+import java.time.*;
+
+@Entity
+@Table(name = "Users")
 public class User {
+        @Id
+        @GeneratedValue
         private long id;
 
+        @Column(length=40, nullable = false, unique = true)
         private String username;
 
+        @Column(length=40, nullable = false)
         private String password;
 
+        @Column(length=80)
         private String name;
 
+        @Column(length=320)
         private String email;
 
         private UserTypes userTypes;
+
+        private LocalDateTime creationDate;
+
+        private LocalDateTime lastLoginDate;
+
+        private boolean active;
 }
