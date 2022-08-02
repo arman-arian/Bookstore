a6dc507935dd3cff36cbedcbf54f93b8c8b4f48e
arman-arian <arman.hassanpoor2000@gmail.com>
2022-08-02 05:31:12 +0430

    fix issues

--- src/main/java/com/tosan/bookstore/daos/UserRepository.java
+++ src/main/java/com/tosan/bookstore/daos/UserRepository.java
@@ -3,8 +3,6 @@ package com.tosan.bookstore.daos;
 import com.tosan.bookstore.models.User;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
 }
--- src/main/java/com/tosan/bookstore/models/InvoiceState.java
+++ src/main/java/com/tosan/bookstore/models/InvoiceState.java
@@ -1,10 +1,21 @@
 package com.tosan.bookstore.models;
 
 public enum InvoiceState {
-    Draft,
-    Deleted,
-    Issued,
-    Cancelled,
-    Expired,
-    Paid
+    Draft("پیش نویس"),
+    Deleted("حذف شده"),
+    Issued("صادر شده"),
+    Cancelled("انصراف داده  شده"),
+    Expired("منقضی شده"),
+    Paid("پرداخت شده");
+
+    private final String description;
+
+    InvoiceState(String description)
+    {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
