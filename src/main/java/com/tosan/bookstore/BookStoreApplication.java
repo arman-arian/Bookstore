f3270dcbc7182bb00aed69230e004da941fe2b41
arman-arian <arman.hassanpoor2000@gmail.com>
2022-08-02 05:27:09 +0430

    fix issues

--- src/main/java/com/tosan/bookstore/BookStoreApplication.java
+++ src/main/java/com/tosan/bookstore/BookStoreApplication.java
@@ -3,15 +3,9 @@ package com.tosan.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class BookStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
-
-//        SpringApplication app = new SpringApplication(BookStoreApplication.class);
-//        app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
-//        app.run(args);
     }
 }
