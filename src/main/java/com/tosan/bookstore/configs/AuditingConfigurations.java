1438777315ecf7c641d30be71da8c52b2fc4b349
arman-arian <arman.hassanpoor2000@gmail.com>
2022-08-22 19:08:00 +0430

    fix issues

--- src/main/java/com/tosan/bookstore/configs/AuditingConfigurations.java
+++ src/main/java/com/tosan/bookstore/configs/AuditingConfigurations.java
@@ -1,4 +1,4 @@
-package com.tosan.bookstore;
+package com.tosan.bookstore.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/tosan/bookstore/configs/SecurityConfigurations.java
+++ src/main/java/com/tosan/bookstore/configs/SecurityConfigurations.java
@@ -1,4 +1,4 @@
-package com.tosan.bookstore;
+package com.tosan.bookstore.configs;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
